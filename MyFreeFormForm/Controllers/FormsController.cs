49a23502776dc1ea9cdc98d5ef97a473447b0600
Preston Stewart Butler II <38290715+PSButlerII@users.noreply.github.com>
2024-03-10 19:59:58 -0400

    first commit

---
+++ MyFreeFormForm/Controllers/FormsController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyFreeFormForm.Models;
+
+namespace MyFreeFormForm.Controllers
+{
+    public class FormsController : Controller
+    {
+        [HttpGet]
+        public IActionResult SampleForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult SubmitForm(FormFieldModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                // Process form data
+                return RedirectToAction("Success"); // Redirect to a success action/page
+            }
+
+            return View("SampleForm", model); // Return the view with validation messages
+        }
+    }
+
+}
